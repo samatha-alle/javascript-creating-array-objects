55953047dedf5f9c46fb144e8d46f73afad30f7a
samatha-alle <allesamatha311@gmail.com>
2026-03-13 22:03:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let student=[
+    {name:'samatha',age:22,loc:'hyd'},
+    {name:'harshini',age:23,loc:'src'},
+    {name:'sravani',age:20,loc:'knr'},
+    {name:'bhoomika',age:21,loc:'Ap'},
+    {name:'gouthami',age:22,loc:'Ts'}
+];
+console.log(student[2].loc);
+console.log(student[4]);
+console.log(student[0].name);
